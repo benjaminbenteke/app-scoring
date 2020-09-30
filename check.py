26b2e7bdeeeb052528269fa09c5e64e20906f56a
Benjamin Benteke Longau <57951412+benjaminbenteke@users.noreply.github.com>
2020-09-30 17:26:14 +0000

    Add files via upload

---
+++ check.py
@@ -0,0 +1,29 @@
+import matplotlib
+print(matplotlib.__version__)
+from flask import Flask
+import flask as fl
+print(fl.__version__)
+import gunicorn as g
+print(g.__version__)
+
+import click as cl
+print(cl.__version__)
+
+import itsdangerous as it
+print(it.__version__)
+
+import Markupsafe as m
+print(m.__version__)
+import Jinja2 as j
+print(j.__version__)
+
+import Werkzeug as w
+print(w.__version__)
+import joblib
+print(joblib.__version__)
+import sklearn as sk
+print(sk.__version__)
+import numpy
+print(numpy.__version__)
+import pandas
+print(pandas.__version__)
